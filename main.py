a5f1de714db6db883122b63f2a054fde070f53a5
Will Fuger <wfuger@gmail.com>
2020-03-07 16:06:45 -0600

    function stubs

---
+++ main.py
@@ -0,0 +1,37 @@
+import dijkstras
+import kinematics
+
+
+# takes in grid size
+# calculates cell size
+def get_grid()
+    return None
+
+# takes in obstacles
+def get_obstacles()
+    return None
+
+# validate pitstops are not in obstacles
+
+# remove surrounding cells around obstacles based on grid size
+def remove_obstacle_cells(obstacles):
+    return None
+
+def construct_grid()
+    return None
+
+def construct_graph_from_grid()
+    return None
+
+def get_path(pt_a, pt_b)
+    return None
+
+# takes in start/end
+def get_start_and_goal()
+    return None
+
+# takes in pitstops
+def get_pitstops()
+    return None
+
+
