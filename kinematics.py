f20b8d84abebabd131a42ec216e53bc0937b7e31
Will Fuger <wfuger@gmail.com>
2020-03-13 07:54:17 -0500

    wheel rotations

--- kinematics.py
+++ kinematics.py
@@ -1,33 +1,69 @@
 from math import sqrt, acos, pi
 
-# grid -> x, y, theta (c space)
-# x, y, theta -> wheel rots (w space)
+# x, theta -> wheel rots (w space)
 class Kinematics():
-    def __init__(self, cell_size):
+    # all units in meters
+    def __init__(self, cell_size, axle_length, wheel_radius):
         self.cell_size = cell_size
+        self.axle_length = axle_length
+        self.wheel_radius = wheel_radius
 
 
+    # grid -> x, theta (c space)
     def grid_path_to_cspace(self, path):
         moves = list()
         current_theta = 0
         for i in range(1, len(path)):
-            ax, ay = path[i - 1] 
-            bx, by = path[i] 
-            x, y, theta
-            if ax == bx and ay == by:
-                x, y = sqrt(2*(self.cell_size ** 2))
-                # current_theta
+            a = path[i - 1] 
+            b = path[i] 
+            x = 1
+            theta = self.get_rotation(a, b, current_theta)
+            if a[0] != b[0] and a[1] != b[1]:
+                x = sqrt(2)
+            translation_m = self.get_translation(x)
+            moves.append((translation_m, theta))
+            current_theta = self.new_orientation(a, b)
+
         return moves
 
 
-    def get_rotation(self, a, b, orientation):
+    def rotation_to_wheel_rotation(self, theta):
+        """
+        Takes rotation desired of robot in radians
+        returns (theta_left, theta_right)
+        """
+        if theta == 0:
+            return (0, 0)
+        elif theta > 0:
+            theta_r = (self.axle_length/2)*(theta/self.wheel_radius)
+            return (-theta_r, theta_r)
+        else:
+            theta_r = (self.axle_length/2)*(theta/self.wheel_radius)
+            return (theta_r, -theta_r)
+
+
+    def translation_to_wheel_rotation(self, x):
+        theta_r = x/self.wheel_radius
+        return (theta_r, theta_r)
+
+
+    def get_translation(self, grid_units):
+        return grid_units * self.cell_size
+
+
+    def new_orientation(self, a, b):
         # dot_prod(a,b) = mag(a)*mag(b)*cos(theta)
         vec = self.get_vector(a, b)
         # cos(theta) = dot_prod(v, i^)/mag(v)
         cos_theta = self.dot_product(vec, (1, 0))/(self.magnitude(vec))
         rad = acos(cos_theta)
+        return rad
 
+
+    def get_rotation(self, a, b, orientation):
+        rad = self.new_orientation(a, b)
         # y direction of vector is negative so in quad 3/4
+        vec = self.get_vector(a, b)
         if vec[1] < 0:
             rad *= -1
 
@@ -41,6 +77,7 @@ class Kinematics():
 
     def get_vector(self, a, b):
         return (b[0]-a[0], b[1]-a[1])
+
 
     def magnitude(self, a):
         return sqrt(a[0]**2 + a[1]**2)
--- test_kinematics.py
+++ test_kinematics.py
@@ -1,16 +1,43 @@
 import pytest
-from math import pi
+from math import pi, sqrt
 from kinematics import * 
 
 grid = [(1,2), (1,3), (2,4), (3,4)]
-wspace = [(0, 1, 0), (1.4, 1.4, 0.7), (1, 0, -0.7)]
+wspace = [(.1, pi/2), (sqrt(2)/10, -pi/4), (.1, -pi/4)]
 
-def test_grid_path_to_workspace():
-    k = Kinematics(0.1)
-    workspace = k.grid_path_to_workspace(grid)
-    assert workspace == wspace
+@pytest.mark.grid_to_cspace
+def test_grid_path_to_cspace():
+    k = Kinematics(0.1, 0.15, 0.05)
+    workspace = k.grid_path_to_cspace(grid)
+    print(workspace)
+    for i, move in enumerate(workspace):
+        expected = wspace[i]
+        assert move[0] == pytest.approx(expected[0])
+        assert move[1] == pytest.approx(expected[1])
 
 
+@pytest.mark.rot_wheel_rotations
+def test_rotation_to_wheel_rotation():
+    axle_length = 0.15
+    wheel_radius = 0.05
+    k = Kinematics(0.1, axle_length, wheel_radius)
+    theta_axle = pi/2
+    theta_l, theta_r = k.rotation_to_wheel_rotation(theta_axle)
+    print(f"theta l {theta_l}")
+    print(f"theta r {theta_r}")
+    aboot = (theta_axle*(axle_length/2))/wheel_radius
+    assert theta_r == pytest.approx(aboot)
+    assert theta_l == pytest.approx(-aboot)
+
+
+@pytest.mark.trans_wheel_rotations
+def test_translation_to_wheel_rotation():
+    k = Kinematics(0.1, 0.15, 0.05)
+    theta_l, theta_r = k.translation_to_wheel_rotation(5)
+    print(f"theta l {theta_l}")
+    print(f"theta r {theta_r}")
+    assert theta_r == 100
+    assert theta_l == 100
 
 # orientations
 s = (3*pi)/2
@@ -48,49 +75,14 @@ circle_test = {
 
 @pytest.mark.rotation
 def test_get_rotation():
-    k = Kinematics(0.1)
+    k = Kinematics(0.1, 0.15, 0.05)
     for o, vecs in circle_test.items():
-        print(f"vecs {vecs}")
         for i, vec in enumerate(vecs):
             a = vec[0]
             b = vec[1]
-            print(f"i {i}")
-            print(f"expect {around[i]}")
             if around[i] == pi:
                 assert abs(k.get_rotation(a, b, o)) == pytest.approx(around[i])
             else:
                 assert k.get_rotation(a, b, o) == pytest.approx(around[i])
 
 
-    # # go from s to ne
-    # a = v_ne[0]
-    # b = v_ne[1]
-    # o = s
-    # assert k.get_rotation(a, b, o) == pytest.approx((3*pi)/4)
-    # # go from s to nw
-    # a = v_nw[0]
-    # b = v_nw[1]
-    # assert k.get_rotation(a, b, o) == pytest.approx(-(3*pi)/4)
-    # # go from s to n
-    # a = v_n[0]
-    # b = v_n[1]
-    # assert k.get_rotation(a, b, o) == pytest.approx(-pi)
-    # # go from s to e
-    # a = v_e[0]
-    # b = v_e[1]
-    # assert k.get_rotation(a, b, o) == pytest.approx(pi/2)
-    # # go from s to w
-    # a = v_w[0]
-    # b = v_w[1]
-    # assert k.get_rotation(a, b, o) == pytest.approx(-pi/2)
-    # # go from s to sw
-    # a = v_sw[0]
-    # b = v_sw[1]
-    # assert k.get_rotation(a, b, o) == pytest.approx(-pi/4)
-    # # go from s to se
-    # a = v_se[0]
-    # b = v_se[1]
-    # assert k.get_rotation(a, b, o) == pytest.approx(pi/4)
-
-def test_workspace_to_cspace():
-    pass
