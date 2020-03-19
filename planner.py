fix edge case of negative orientation and pi rotation, weights of diagonals 1.4

planner.py
@@ -82,23 +82,23 @@ def build_matrix(m,obstacles,robot_margin,cell_size,grid_size,width_size):
             if(i>0 and j>0):
                 bottomleft=(i-1)*grid_size+j-1
                 if(env[i-1,j-1]):
-                    m[center,bottomleft]=1
-                    m[bottomleft,center]=1
+                    m[center,bottomleft]=1.4
+                    m[bottomleft,center]=1.4
             if(j>0 and i+1<width_size):
                 upleft=(i+1)*grid_size+j-1
                 if(env[i+1,j-1]):
-                    m[center,upleft]=1.5
-                    m[upleft,center]=1.5
+                    m[center,upleft]=1.4
+                    m[upleft,center]=1.4
             if(i+1<width_size and j+1 <grid_size):
                 upright=(i+1)*grid_size+j+1
                 if(env[i+1,j+1]):
-                    m[center,upright]=1.5
-                    m[upright,center]=1.5
+                    m[center,upright]=1.4
+                    m[upright,center]=1.4
             if(i>0 and j+1<grid_size):
                 bottomright=(i-1)*grid_size+j+1
                 if(env[i-1,j+1]):
-                    m[center,bottomright]=1.5
-                    m[bottomright,center]=1.5
+                    m[center,bottomright]=1.4
+                    m[bottomright,center]=1.4
                 
     return m
 
@@ -269,33 +269,85 @@ if __name__ == "__main__":
     
     
     #test shortest path
-    obstacles1=np.array([[20,20,4.5,6.2],[2.6,2.6,4,4]],dtype=np.float)
+    # obstacles1=np.array([[20,20,4.5,6.2],[2.6,2.6,4,4]],dtype=np.float)
     #obstacles2=
+    # robot_margin=0.1
+    # grid_length=10
+    # grid_size=100
+    # obs=np.array([[20,20,4.5,6.2],[2.6,2.6,4,4]],dtype=np.float)
+    # obs = [[[.5, 0, 0], [1, .5, .5]], [[-1, 0, 0], [-.5, .5, .5]]]
+    # obs = np.array([[0.5, 1, 1.5, 2], [.5, 8, 1.5, 9]], dtype=np.float)
+    # planner=Planner(obs,robot_margin,grid_length,grid_size)
+    # planner.build_graph()
+    # start = [0.05, 0.025]
+    # end = [5.715, 5.735]
+    L=10
+    W=9
     robot_margin=0.1
-    grid_length=10
+
     grid_size=100
     # obs=np.array([[20,20,4.5,6.2],[2.6,2.6,4,4]],dtype=np.float)
     # obs = [[[.5, 0, 0], [1, .5, .5]], [[-1, 0, 0], [-.5, .5, .5]]]
-    obs = np.array([[0.5, 1, 1.5, 2], [.5, 8, 1.5, 9]], dtype=np.float)
-    planner=Planner(obs,robot_margin,grid_length,grid_size)
+    # obs = [[],\
+
+    obs = np.array([[1, 1, 2, 2],
+        [5, 1,6, 3],
+        [1, 6, 2, 8],
+        [7, 7, 8, 10],
+        [8, 2, 9, 3]], dtype=np.float)
+    # obs = np.array([[0.5, 1, 1.5, 2], [3,3.5,3.5,2]], dtype=np.float)
+    planner=Planner(obs,robot_margin,grid_length=L,grid_width=W,grid_size=grid_size)
     planner.build_graph()
-    start = [0.05, 0.025]
-    end = [5.715, 5.735]
-    path=planner.path(start,end)
-    if path != -1:
-        with open("cell_path.txt", "w") as f:
-            for cell in path:
-                f.write(f"{cell}\n")
-        k = Kinematics(0.1, 0.12, 0.028)
-        x_thetas = k.grid_path_to_cspace(path, start, end)
-        with open("x_theta_path.txt", "w") as f:
-            for cell in x_thetas:
-                f.write(f"{cell}\n")
+    start = [5, 5]
+    end = [1., 5.]
+    points = [[1., 0.5], [3., 1.], [6., 9.],end]
+    # [1., 0.5], [3., 1.], [6., 9.], [1., 5.]
+    pts = [start, [1., 0.5], [3., 1.], [6., 9.], end]
+    paths=planner.multiple_dijk(start,points)
+    # paths = [[
+    #     (50, 50), (49, 49), (48, 48), (47, 47), (46, 46), (45, 45), (44, 44), (43, 43), (42, 42), (41, 41), (40, 40), (39, 39), (38, 38), (37, 37), (36, 36), (35, 35), (34, 34), (34, 33), (34, 32), (34, 31), (34, 30), (34, 29), (34, 28), (33, 27), (33, 26), (32, 25), (31, 24), (30, 23), (30, 22), (29, 21), (28, 20), (27, 19), (26, 18), (26, 17), (26, 16), (25, 15), (24, 14), (24, 13), (23, 12), (23, 11), (23, 10), (23, 9), (22, 8), (21, 7), (20, 7), (19, 7), (18, 7), (17, 7), (16, 7), (15, 7), (14, 7), (13, 7), (12, 7), (11, 6), (10, 5)]
+    # ]
+    #     [ (30, 1), (30, 2), (30, 3), (30, 4), (30, 5), (30, 6), (30, 7), (30, 8), (30, 9), (30, 10), (29, 11), (28, 12), (27, 13), (26, 14), (25, 15), (24, 16), (23, 17), (22, 18), (21, 19), (20, 20) ],
+    #     [ (20, 20), (20, 21), (20, 22), (20, 23), (20, 24), (20, 25), (20, 26), (20, 27), (20, 28), (20, 29), (20, 30), (20, 31), (20, 32), (20, 33), (20, 34), (20, 35), (20, 36), (20, 37), (20, 38), (20, 39), (20, 40) ],
+    #     [ (20, 40), (21, 40), (22, 40), (23, 40), (24, 40), (25, 40), (26, 40), (27, 40), (28, 40), (29, 40), (30, 40), (31, 40), (32, 40), (33, 40), (34, 40), (35, 40), (36, 40), (37, 40), (38, 40), (39, 40), (40, 40), (41, 41), (42, 42) ],
+    #     # [ (42, 42), (43, 43), (44, 44), (45, 45), (46, 46), (47, 47), (48, 48), (49, 49), (50, 50), (51, 51), (52, 52), (53, 53), (54, 54), (55, 55) ]
+    # ]
+    # path=planner.path(start,end)
+    k = Kinematics(0.1, 0.12, 0.028)
+    # with open('paths.txt', 'w') as f:
+    #     for path in paths:
+    #         f.write(", ".join([f"({left}, {right})" for left, right in path]))
+    #         f.write("\n")
+    rots = list()
+    # if paths != -1:
+    for i in range(len(paths)):
+        print(f"start pts {pts[i]}")
+        print(f"end pts {pts[i+1]}")
+        print(f"start path {paths[i][0]}")
+        print(f"end path {paths[i][-1]}")
+        x_thetas = k.grid_path_to_cspace(paths[i], pts[i], pts[i+1])
         wheel_rotations = k.cspace_to_wheel_rotations()
-        with open("path.txt", "w") as f:
-            for rot in wheel_rotations:
-                left, right = rot
-                f.write(f"{left} {right}\n")
+        rots += wheel_rotations
+
+    # print(f"rots {rots}")
+            
+    with open("path.txt", "w") as f:
+        for rot in rots:
+            left, right = rot
+            f.write(f"{left} {right}\n")
+        # with open("cell_path.txt", "w") as f:
+        #     for cell in path:
+        #         f.write(f"{cell}\n")
+        
+        # x_thetas = k.grid_path_to_cspace(path, start, end)
+        # with open("x_theta_path.txt", "w") as f:
+        #     for cell in x_thetas:
+        #         f.write(f"{cell}\n")
+        # wheel_rotations = k.cspace_to_wheel_rotations()
+        # with open("path.txt", "w") as f:
+        #     for rot in wheel_rotations:
+        #         left, right = rot
+        #         f.write(f"{left} {right}\n")
     
     
     